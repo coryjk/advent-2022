d9bb5b77997e3e42d92dc464411371b2f7afe19c
coryjk <36049396+coryjk@users.noreply.github.com>
2022-12-07 02:41:14 -0800

    Day 7 part 1

---
+++ internal/solutions/day7.go
@@ -0,0 +1,145 @@
+package solutions
+
+import (
+	"container/list"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func Day7() {
+	p := Problem{
+		InputPath: "input/day7.txt",
+	}
+	var input []string = Input(p)
+	fmt.Println(day7Part1(&input))
+	fmt.Println(day7Part2(&input))
+}
+
+type file struct {
+	name     string
+	parent   *file
+	children *list.List
+	size     int
+}
+
+func day7Part1(input *[]string) string {
+	// init root
+	root := file{
+		name:     "/",
+		parent:   nil,
+		children: list.New(),
+		size:     0,
+	}
+
+	// populate file structure based on stdout
+	crawl(&root, input)
+
+	// find deletion dirs and their sum sizes
+	dirsForDeletion, _ := findDirsForDeletion(&root, 100000)
+	size := 0
+	for _, dirs := range *dirsForDeletion {
+		size += dirs.size
+	}
+	return strconv.Itoa(size)
+}
+
+func day7Part2(input *[]string) string {
+	return ""
+}
+
+func crawl(root *file, stdout *[]string) {
+	current := root
+	for i, line := range *stdout {
+		// skip i == 0 since root already initialized
+		if i == 0 {
+			continue
+		}
+		split := strings.Split(line, " ")
+
+		// indicates some command
+		if split[0] == "$" {
+			command := split[1]
+			switch command {
+			case "cd":
+				// expecting 1 arg
+				arg := split[2]
+				switch arg {
+				case "..":
+					// navigate to parent
+					current = current.parent
+				case "/":
+					// navigate to root
+					current = root
+				default:
+					// navigate to child dir, assume it is populated by now
+					target := childByName(arg, current.children)
+					current = target
+				}
+			case "ls":
+				// expecting no args...
+			}
+		} else {
+			// indicates display of some information, need to further
+			// populate file structure with this info
+
+			// initialize dir and populate
+			if split[0] == "dir" {
+				current.children.PushBack(
+					newFile(split[1], current, 0),
+				)
+			} else {
+				// got file info, populate
+				size, _ := strconv.Atoi(split[0])
+				current.children.PushBack(
+					newFile(split[1], current, size),
+				)
+				// only accumulate size 1 level up at dir level
+				current.size += size
+			}
+		}
+	}
+}
+
+func newFile(name string, parent *file, size int) *file {
+	return &file{name, parent, list.New(), size}
+}
+
+func childByName(name string, files *list.List) *file {
+	for i := files.Front(); i != nil; i = i.Next() {
+		// list stores file pointers
+		file := i.Value.(*file)
+		if file.name == name {
+			return file
+		}
+	}
+	return nil
+}
+
+func findDirsForDeletion(current *file, max int) (*[]*file, int) {
+	// file or empty dir
+	if current.children.Len() == 0 {
+		return nil, current.size
+	} else {
+		sum := 0
+		var dirsForDeletion []*file
+
+		// dir with children, recurse
+		for i := current.children.Front(); i != nil; i = i.Next() {
+			dirs, size := findDirsForDeletion(i.Value.(*file), max)
+
+			// accumulate
+			sum += size
+			if dirs != nil {
+				dirsForDeletion = append(dirsForDeletion, *dirs...)
+			}
+		}
+
+		// if current dir less than max size, it is candidate for deletion
+		current.size = sum
+		if sum <= max {
+			dirsForDeletion = append(dirsForDeletion, current)
+		}
+		return &dirsForDeletion, sum
+	}
+}
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	solutions.Day6()
+	solutions.Day7()
 }
